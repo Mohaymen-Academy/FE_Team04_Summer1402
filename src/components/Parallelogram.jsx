3d2a72dc0886aa18dd043ea274b94de34df31fd8
Mojtaba Bashari <m-bashari@outlook.com>
2023-07-16 16:33:30 +0330

    Modify: Parallelogram

--- src/components/Parallelogram.jsx
+++ src/components/Parallelogram.jsx
@@ -1,10 +1,10 @@
 function Parallelogram(props) {
   return (
     <a
-      className="h-20 p-6 bg-red-300 -skew-x-20 inline-flex justify-center items-center hover:bg-red-400 transition"
+      className="h-20 p-6 bg-red-300 -skew-x-20 inline-flex justify-center items-center hover:bg-red-400 transition text-2xl text-white"
       href={props.href}
     >
-      {props.children}
+      <div className="skew-x-20">{props.children}</div>
     </a>
   );
 }
