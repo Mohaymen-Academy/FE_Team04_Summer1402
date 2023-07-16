9507e2ac0b7d7f8ba7811b4b44daff816373cdb6
Mojtaba Bashari <m-bashari@outlook.com>
2023-07-16 17:17:59 +0330

    Modify: header, compliting header items

--- src/components/Parallelogram.jsx
+++ src/components/Parallelogram.jsx
@@ -1,10 +1,10 @@
 function Parallelogram(props) {
   return (
     <a
-      className="h-20 p-6 bg-red-300 -skew-x-20 inline-flex justify-center items-center hover:bg-red-400 transition"
+      className="h-20 p-6 bg-red-400 -skew-x-20 inline-flex justify-center items-center hover:bg-red-500 transition mx-1 text-2xl text-white"
       href={props.href}
     >
-      <p className="skew-x-20 text-xl">{props.text}</p>
+      <div className="skew-x-20">{props.children}</div>
     </a>
   );
 }
