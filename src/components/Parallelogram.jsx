7150b09057af5cb7fe0d451f5b1be8be38b49d49
Mojtaba Bashari <m-bashari@outlook.com>
2023-07-16 16:17:12 +0330

    Modify: parallelogram to get children

--- src/components/Parallelogram.jsx
+++ src/components/Parallelogram.jsx
@@ -4,7 +4,7 @@ function Parallelogram(props) {
       className="h-20 p-6 bg-red-300 -skew-x-20 inline-flex justify-center items-center hover:bg-red-400 transition"
       href={props.href}
     >
-      <p className="skew-x-20 text-xl">{props.text}</p>
+      {props.children}
     </a>
   );
 }
