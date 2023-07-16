bc21d8c7f8eb7eb12db68d40f5e1442db12b7c72
Mojtaba Bashari <m-bashari@outlook.com>
2023-07-16 18:41:43 +0330

    Try Slick

--- src/components/Swip.jsx
+++ src/components/Swip.jsx
@@ -1,32 +1,29 @@
-import { Swiper, SwiperSlide } from "swiper/react";
 import { Card } from "./Card";
 import { data } from "../data/swiper";
-import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
-SwiperCore.use([Pagination, Autoplay]);
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Swip() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <Swiper
-      loop={true}
-      autoplay={{ delay: 3000, disableOnInteraction: false }}
-      grabCursor={true}
-      centeredSlides={true}
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
+    <Slider {...settings}>
       {data.map((data) => (
-        <SwiperSlide>
-          <Card
-            img={data.img}
-            header={data.header}
-            details={data.details}
-            alt={data.alt}
-          />
-        </SwiperSlide>
+        <Card
+          img={data.img}
+          header={data.header}
+          details={data.details}
+          alt={data.alt}
+        />
       ))}
-    </Swiper>
+    </Slider>
   );
 }
 
