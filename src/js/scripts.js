66d431119418539d168b0c6a40c858464eba96da
Mojtaba Bashari <m-bashari@outlook.com>
2023-07-23 15:17:06 +0330

    Modify: sidebars

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,3 +10,18 @@ const settingSection = document.getElementById("toggle-setting");
 settingSection.addEventListener("click", () => {
   setting.classList.toggle("open");
 });
+
+function toggleDropdown() {
+  const dropdownContent = document.querySelector(".dropdown-content");
+  dropdownContent.classList.toggle("show-dropdown");
+}
+
+const buttons = document.querySelectorAll(".setter button");
+const dropdownMenus = document.querySelectorAll(".setter ul");
+
+for (const i in buttons) {
+  buttons[i].addEventListener("click", () => {
+    dropdownMenus[i].classList.toggle("hidden");
+  });
+  console.log(1);
+}
